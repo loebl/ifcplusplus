711fd1cd7eca093b492acc099a37ba1b7c0c627e
Daniel Löber <daniel.loeber@easterngraphics.com>
2018-09-11 16:46:57 +0200

    Add short comment

--- IfcPlusPlus/src/ifcpp/geometry/Carve/ProfileConverter.h
+++ IfcPlusPlus/src/ifcpp/geometry/Carve/ProfileConverter.h
@@ -1056,6 +1056,7 @@ public:
 			simplifyPath( path );
 		}
 	}
+	///\brief Simplifies a path by merging points really close to each other
 	static void simplifyPath( std::vector<vec2>& path )
 	{
 		if( path.size() < 3 )
